09e676ca569908818a0c1dc65adcb23dfaf9be19
Full Name <anis.bastide@gmail.com>
2020-09-30 16:37:15 +0200

    added an entity for the database

---
+++ src/Entity/Team.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeamRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TeamRepository::class)
+ */
+class Team
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $listProject = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getListProject(): ?array
+    {
+        return $this->listProject;
+    }
+
+    public function setListProject(?array $listProject): self
+    {
+        $this->listProject = $listProject;
+
+        return $this;
+    }
+}
