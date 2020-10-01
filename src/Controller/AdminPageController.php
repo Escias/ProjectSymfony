ee5ac821d8581435bed84632ee4d6d9cb2f76a47
Escias <ljimmy98@hotmail.fr>
2020-10-01 13:41:47 +0200

    admin page & table

---
+++ src/Controller/AdminPageController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+Use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use App\Service\DataBaseServices;
+
+class AdminPageController{
+
+    /**
+     * @var Environment
+     */
+    private $twig;
+    /**
+     * @var DataBaseServices
+     */
+    private $db;
+
+    public function index()
+    {
+        $titles = [
+            'team' => 'Team',
+            'project' => 'Project',
+            'member' => 'Member',
+        ];
+        $contenu = [
+            [
+                'team' => '1',
+                'project' => [
+                    'Project 1',
+                    'Project 3',
+                ],
+                'member' => [
+                    'Anis',
+                    'Eliott',
+                ],
+            ],
+            [
+                'team' => '1',
+                'project' => [
+                    'Project 1',
+                    'Project 2',
+                ],
+                'member' => [
+                    'Axel',
+                    'Jimmy',
+                ],
+            ]
+        ];
+
+        $team = $this->db->em->getRepository(Team::class);
+        $team1 = $team->find(2);
+        $team1->setListProject([1,2]);
+
+        $this->db->em->persist($team1);
+        $this->db->em->flush();
+
+
+        $content = $this->twig->render("AdminPage/admin.html.twig",
+            [
+                'titles'=>$titles,
+                'entities'=>$contenu,
+            ]);
+        return new Response($content);
+    }
+
+    public function __construct(Environment $twig, DataBaseServices $db)
+    {
+        $this->twig = $twig;
+        $this->db = $db;
+    }
+}
